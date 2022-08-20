45d4cf48b45e4e06db3359b7e6f942c386a115ed
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-08-20 01:03:18 -0300

    Funcionalidade das cartas viradas

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 let contadorQtd = 0
 let contadorNome = 0
+let contador = 0
 let qtdCartas = Number(prompt("Quantas cartas você deseja?"))
 let nomeDasCartas = ["parrot", "cat", "dog", "elephant"]
 nomeDasCartas = nomeDasCartas.sort(()=> Math.random() -0.5)
@@ -13,18 +14,44 @@ while(qtdCartas > 14 || qtdCartas < 4 || qtdCartas%2 == 1) {
 
 
 while (contadorQtd < qtdCartas || contadorNome < nomeDasCartas.length) {
-    cartas.innerHTML = cartas.innerHTML +`<li><div onclick="virarCarta(this)" class="card ${nomeDasCartas[contadorNome]}"><img src="img/front 1.png" alt=""></div><!--card--></li>`
+    cartas.innerHTML = cartas.innerHTML +`<li><div onclick="virarCarta(this)" class="card ${nomeDasCartas[contadorNome]}"><div><img src="img/front 1.png" alt=""></div><!--card--><div/></li>`
     contadorQtd++
     contadorNome++
+}
+
+function virarCarta(card) {
+    
+    
+    if(card.classList.contains("rotate") ) {
+        return false      
+    }
+    if(contador > 1 ) {
+        return false
+    }
+
+    contador++
+    console.log(contador)
+
+    card.classList.toggle(`rotate`)
+    card.classList.toggle(`ctt${contador}`)
+    card.classList.toggle("none")
+
+    if (card.classList.contains("dog")) {
+        card.classList.toggle("jake")
+    }
+
+    let id = setInterval(temporizador, 5000)
+
+    function temporizador() {
+        card.classList.toggle("rotate")
+        card.classList.remove("none")
+        card.classList.remove("jake")
+        contador--
+        clearInterval(id)
+        console.log(contador)
+    }
 
     
 }
 
-function virarCarta(card) {
-    card.classList.toggle("rotate")
-    if(card.classList.contains("rotate")) {
-        card.innerHTML = `<li><div onclick="virarCarta(this)" class="card ${nomeDasCartas[contadorNome]}"></div><!--card--></li>`
-    } else {
-        card.innerHTML = `<li><div onclick="virarCarta(this)" class="card ${nomeDasCartas[contadorNome]}"><img src="img/front 1.png" alt=""></div><!--card--></li>`
-    }
-}
+
